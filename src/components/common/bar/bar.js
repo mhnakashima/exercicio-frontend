995daf39dc3230f3cae4df74c02eee328524857e
michael haruo nakashima <mhnakashima@gmail.com>
2020-08-16 23:05:41 -0300

    bar css

--- src/components/common/bar/bar.js
+++ src/components/common/bar/bar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Title from "../title/title";
 
+import './bar.scss';
+
 class Bar extends React.Component {
   render() {
     return (
       <div className="bar">
           <div className="bar__description">
-            <Title title={this.props.value.name} />
-            <span>{this.props.value.level}</span>
+            <Title className='bar__name' title={this.props.value.name} />
+            <span  className='bar__level'>{this.props.value.level}</span>
           </div>
           <div className='bar__container'>
               <div className='bar__value' style={{ "width": (this.props.value.score + '%') }}></div>
